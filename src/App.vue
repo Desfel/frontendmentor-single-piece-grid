1d5c5ae147b457726d4f3d1106ce66ad8847fb6b
Desfel <wil.southiphonh@gmail.com>
2021-09-22 21:33:02 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,122 @@
+<template>
+  <div id="app">
+    <div class="main-wrapper">
+      <router-view />
+    </div>
+
+    <new-content-available-toastr
+      v-if="newContentAvailable"
+      class="new-content-available-toastr"
+      :refreshing-app="refreshingApp"
+      @refresh="serviceWorkerSkipWaiting"
+    ></new-content-available-toastr>
+    <apple-add-to-home-screen-modal
+      v-if="showAddToHomeScreenModalForApple"
+      class="apple-add-to-home-screen-modal"
+      @close="closeAddToHomeScreenModalForApple(false)"
+    >
+    </apple-add-to-home-screen-modal>
+  </div>
+</template>
+<script>
+import NewContentAvailableToastr from '@/components/NewContentAvailableToastr'
+import AppleAddToHomeScreenModal from '@/components/AppleAddToHomeScreenModal'
+import { mapState, mapActions, mapGetters } from 'vuex'
+
+export default {
+  components: { NewContentAvailableToastr, AppleAddToHomeScreenModal },
+  computed: {
+    ...mapGetters('app', ['newContentAvailable']),
+    ...mapState('app', ['showAddToHomeScreenModalForApple', 'refreshingApp'])
+  },
+  methods: mapActions('app', [
+    'closeAddToHomeScreenModalForApple',
+    'serviceWorkerSkipWaiting'
+  ])
+}
+</script>
+
+<style lang="scss">
+@import '@/theme/variables.scss';
+
+body {
+  margin: 0;
+
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    font-weight: 500;
+    text-decoration: none;
+  }
+
+  h1,
+  h2,
+  h3,
+  p,
+  a {
+    margin: 0;
+    font-style: normal;
+    font-family: $textFont;
+    font-weight: 400;
+  }
+
+  p,
+  a {
+    font-size: 16px;
+    line-height: 26px;
+    letter-spacing: -0.2px;
+  }
+
+  p {
+    @media (max-width: 767px) {
+      font-size: 14px;
+      line-height: 26px;
+      letter-spacing: -0.175px;
+    }
+  }
+
+  #app {
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 16px;
+    color: #2c3e50;
+
+    .new-content-available-toastr {
+      position: absolute;
+      bottom: 10px;
+      right: 10px;
+    }
+
+    .apple-add-to-home-screen-modal {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      top: 0;
+      left: 0;
+      height: fit-content;
+      width: fit-content;
+      margin: auto;
+      z-index: 1000;
+    }
+
+    .main-wrapper {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin: 0;
+      height: 100vh;
+      padding: 20px;
+      background: #e6eff5;
+
+      .page-wrapper {
+        width: 100%;
+        max-width: 1440px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,199 @@
+<template>
+  <div class="page-wrapper">
+    <div class="card-content">
+      <div class="card-section card-top">
+        <h1 class="title">Join our community</h1>
+
+        <h2 class="small-title">30-day, hassle-free money back guarantee</h2>
+
+        <p>Gain access to our full library of tutorials along with expert code reviews.
+          Perfect for any developers who are serious about honing their skills.</p>
+      </div>
+
+      <div class="card-bottom">
+        <div class="card-section bottom-left">
+          <h2 class="small-title">Monthly Subscription</h2>
+
+          <p class="price-line">$29 <span>per month</span></p>
+
+          <p>Full access for less than &dollar;1 a day</p>
+
+          <a href="#" class="cta-btn">Sign Up</a>
+        </div>
+        <div class="card-section bottom-right">
+          <h2 class="small-title">Why Us</h2>
+
+          <ul>
+            <li>Tutorials by industry experts</li>
+            <li>Peer & expert code review</li>
+            <li>Coding exercises</li>
+            <li>Access to our GitHub repos</li>
+            <li>Community forum</li>
+            <li>Flashcard decks</li>
+            <li>New videos every week</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  head() {
+    return {
+      title: {
+        inner: 'Home'
+      },
+      meta: [
+        {
+          name: 'description',
+          content: `${this.appTitle} home page`,
+          id: 'desc'
+        }
+      ]
+    }
+  },
+  computed: mapState('app', ['appTitle'])
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/theme/variables.scss';
+
+.page-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 40px;
+
+  .card-content {
+    //width: 45%;
+    max-width: 635px;
+    border-radius: 8px;
+    background: $light;
+    box-shadow: 0 15px 30px rgba(0, 81, 171, 0.148847);
+
+    .title,
+    .small-title {
+      font-weight: 700;
+    }
+
+    .title {
+      font-size: 24px;
+      line-height: 28px;
+      letter-spacing: -0.3px;
+      color: $primaryColor1;
+
+      @media (max-width: 767px) {
+        font-size: 20px;
+        line-height: 23px;
+        letter-spacing: -0.25px;
+      }
+    }
+
+    .small-title {
+      font-size: 18px;
+      line-height: 21px;
+      letter-spacing: -0.225px;
+    }
+
+    .cta-btn {
+      display: inline-block;
+      margin-top: 26px;
+      width: 100%;
+      text-align: center;
+      padding: 15px 0 14px;
+      background: $primaryColor2;
+      color: $light;
+      font-weight: 700;
+      border-radius: 5px;
+      box-shadow: 0 10px 10px rgba(0, 0, 0, 0.0973011);
+    }
+
+    .card-section {
+      padding: 40px;
+    }
+
+    .card-top {
+      .small-title {
+        margin: 24px 0 11px;
+        color: $primaryColor2;
+
+        @media (max-width: 767px) {
+          font-size: 15px;
+          line-height: 20px;
+          letter-spacing: -0.1875px;
+        }
+      }
+
+      p {
+        color: $neutralColor2;
+      }
+    }
+
+    .card-bottom {
+      display: flex;
+      align-items: stretch;
+      background: $primaryColor1;
+      color: $light;
+
+      @media (max-width: 767px) {
+        flex-direction: column;
+      }
+
+      .card-section {
+        width: 50%;
+
+        @media (max-width: 767px) {
+          width: 100%;
+        }
+      }
+
+      .bottom-left {
+        .price-line {
+          display: flex;
+          align-items: center;
+          margin: 18px 0 4px;
+          font-size: 32px;
+          font-weight: 700;
+          line-height: 37px;
+          letter-spacing: -0.4px;
+
+          span {
+            margin-left: 11px;
+            font-size: 16px;
+            line-height: 26px;
+            font-weight: 400;
+            letter-spacing: -0.2px;
+            mix-blend-mode: normal;
+            opacity: 0.5;
+          }
+        }
+      }
+
+      .bottom-right {
+        background: rgba($light, .15);
+
+        ul {
+          margin: 18px 0 0;
+          list-style: none;
+          padding: 0;
+
+          li {
+            font-family: $textFont;
+            font-size: 14px;
+            line-height: 20px;
+            letter-spacing: -0.175px;
+            mix-blend-mode: normal;
+            opacity: 0.75;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
